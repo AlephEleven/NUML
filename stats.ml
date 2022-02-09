4a57329e996e418096ecfb468ad458dd85e4609d
AlephEleven <your_email@abc.com>
2022-02-08 19:06:23 -0500

    plotting (gen xy and desmos xy)

--- stats.ml
+++ stats.ml
@@ -1,3 +1,7 @@
+#use "topfind";;
+#require "lwt";;
+
+
 module NUML = struct
 
 (* STATS *)
@@ -64,60 +68,18 @@ let indx l n = List.nth l n
 let indx2d l n_x n_y = List.nth (List.nth l n_x) n_y
 ;;
 
-(* GRAPHICS *)
-
-let rec gen_row n =
-match n with
-| 0 -> []
-| _ -> [" "]@gen_row (n-1)
-;;
-
-let rec gen_sq x y =
-match x with
-| 0 -> []
-| _ -> (gen_row y)::gen_sq (x-1) y
-;;
-
-let rec set_row' l indx key cur =
+let rec gen_xy l f =
 match l with
 | [] -> []
-| h::t when indx=cur -> key::set_row' t indx key (cur+1)
-| h::t -> h::set_row' t indx key (cur+1)
+| h::t -> [(h, (f h))]@gen_xy t f
 ;;
 
-let set_row l indx key = set_row' l indx key 0
-;;
-
-let rec set_sq' l indx_x indx_y key cur_x =
-match l with
-| [] -> []
-| h::t when cur_x = indx_x -> (set_row h indx_y key)::set_sq' t indx_x indx_y key (cur_x+1)
-| h::t -> h::set_sq' t indx_x indx_y key (cur_x+1)
-;;
-
-let set_sq l indx_x indx_y key = set_sq' l indx_x indx_y key 0
-;;
-
-let is_btwn a b key = if key > a && key <= b then true else false
+let rec desmos_xy pts =
+match pts with
+| [] -> nan
+| (x,y)::t -> (Printf.printf "(%4f,%4f)," x y); (desmos_xy t)
 ;;
 
 
-let rec linbtwn' linsp key indx =
-match linsp with
-| [] -> -1
-| x::y::t when (is_btwn x y key) -> indx
-| h::t -> linbtwn' t key (indx+1)
-;;
-
-let linbtwn linsp key = linbtwn' linsp key 0
-;;
-
-(* 
-Example: Plotting 0.5 on a line between -1 and 1
-
-        Graphic [0 - 10], Check where 0.5 is between in linspace -1 to 1 w/ 10 cuts, set a * there
-set_row (gen_row 10) (linbtwn (linspace (-1.) 1. 10) 0.5) "*"
-;;
- *)
 
  end
